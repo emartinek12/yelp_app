42729d79530d83d46d64c6efb4650c1ebaaca1dc
Emily Martinek <emily.martinek12@gmail.com>
2021-11-10 12:38:27 +0100

    validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_104328) do
+ActiveRecord::Schema.define(version: 2021_11_10_110345) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -24,8 +24,11 @@ ActiveRecord::Schema.define(version: 2021_11_10_104328) do
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
+    t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
